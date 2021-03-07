80ff896a8eb9e8ad92e19811e38979bfdb102fcb
Antoine de Prieelle <antoine_@live.nl>
2021-03-07 13:01:25 +0100

    Add react-router-dom Links to react component Home and update padding product-title in ProductDetails.scss

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,31 +5,26 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
-      {/* <div className="home-title">Home</div>
-
-      <Link to="shop/all">
-        All
-      </Link>
-      <Link to="shop/men">
-        Men
-      </Link>
-      <Link to="shop/women">
-        Women
-      </Link> */}
       <div className="shop-categories">
         <div className="shop-category shop-category-all">
           <div className="shop-category-title">
-            Shop All
+            <Link to="shop/all">
+                Shop All
+            </Link>
           </div>
         </div>
         <div className="shop-category shop-category-women">
           <div className="shop-category-title">
-            Shop Women's
+            <Link to="shop/women">
+              Shop Women's
+            </Link>
           </div>
         </div>
         <div className="shop-category shop-category-men">
          <div className="shop-category-title">
-            Shop Men's
+            <Link to="shop/men">
+              Shop Men's
+            </Link>
           </div>
         </div>
       </div>
